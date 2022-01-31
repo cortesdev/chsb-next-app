c835c84d554d20d86e4502a676372ad7ec0bf7c2
Ricardo Côrtes <giralander@hotmail.com>
2022-01-31 15:25:12 +0100

    Implement grid to list

--- design-system/organisms/About/AboutFeaturesList.tsx
+++ design-system/organisms/About/AboutFeaturesList.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, Stack } from "@mui/material";
+import { Button, Card, Grid, Stack } from "@mui/material";
 import Image from "next/image";
 import React, { FC } from "react";
 import Typography from "../../atoms/Typography";
@@ -13,7 +13,7 @@ export const AboutFeaturesList: FC = () => {
         direction="column"
         sx={{
           display: "flex",
-          alignItems: "center",
+          alignItems: "baseline",
           margin: "0 auto",
         }}
       >
@@ -33,30 +33,53 @@ export const AboutFeaturesList: FC = () => {
       </Stack>
 
       {AboutFeatureItems.map((items, index) => (
-        <Grid item sm={12} md={4}>
-          <Stack direction="row" mb={2}>
-            <Image width="40" height="40" src={items.url} alt={items.alt} />
+        <Grid item sm={12} md={4} sx={{ flexWrap: "wrap", display: "flex" }}>
+          <Card
+            sx={{
+              padding: 4,
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Stack direction="row" mb={2}>
+              <Image width="40" height="40" src={items.url} alt={items.alt} />
 
-            <Typography size="h4" sx={{ marginLeft: 2 }}>
-              {items.headline}
+              <Typography
+                weight="bold"
+                color={theme.colors.primary}
+                size="h6"
+                sx={{ marginLeft: 2 }}
+              >
+                {items.headline}
+              </Typography>
+            </Stack>
+
+            <Typography
+              weight="bold"
+              size="body"
+              sx={{ minHeight: 30, marginBottom: 4 }}
+            >
+              {items.subheadline}
             </Typography>
-          </Stack>
 
-          <Typography weight="bold" size="h6" sx={{ minHeight: 70 }}>
-            {items.subheadline}
-          </Typography>
+            <Typography
+              size="body"
+              sx={{ minHeight: 130, alignItems: "baseline !important" }}
+            >
+              {items.text}
+            </Typography>
 
-          <FlexSpacer minHeight={1} />
+            <FlexSpacer minHeight={2} />
 
-          <Typography size="body" sx={{ minHeight: 100 }}>
-            {items.text}
-          </Typography>
-
-          <FlexSpacer minHeight={2} />
-
-          <Button variant="outlined" color="primary" fullWidth>
-            {items.headline}
-          </Button>
+            <Button
+              variant="outlined"
+              color="primary"
+              fullWidth
+              sx={{ marginTop: "auto" }}
+            >
+              {items.headline}
+            </Button>
+          </Card>
         </Grid>
       ))}
     </>
