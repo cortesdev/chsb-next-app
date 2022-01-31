4c44ee854f3cb7c262c17ecdc7e459d1607f0451
Ricardo Côrtes <giralander@hotmail.com>
2022-01-31 17:56:59 +0100

    Added metatag position

--- pages/Seo.tsx
+++ pages/Seo.tsx
@@ -35,15 +35,18 @@ const Seo: FC = () => {
       <meta
         name="twitter:title"
         content="CHSB Dashboard App - Lorem ipsum dolor sit amet"
-      ></meta>
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:description"
+        content="CHSB Dashboard App - Lorem ipsum dolor sit ametsed do eiusmod tempor incididunt ut labore et dolore magna aliqua. @swissborg"
+      />
+      <meta name="twitter:site" content="@swissborg" />
+      <meta name="twitter:creator" content="@cortesdev" />
       <meta
         name="twitter:image"
         content="https://og-image.vercel.app/**CHSB**%20Dahboard.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg&images=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2Fremojansen%2Flogo.ts%40master%2Fts.svg&images=https%3A%2F%2Fswissborg-website.cdn.prismic.io%2Fswissborg-website%2Faa90e56e-bbcd-43ad-af4f-f30077c1241f_CHSB-shadow-icon.svg%3Fixlib%3DgatsbyFP%26auto%3Dcompress%252Cformat%26fit%3Dmax%26q%3D50%26w%3D400%26h%3D400"
-      ></meta>
-      <meta name="twitter:card" content="summary_large_image"></meta>
-      <meta name="twitter:description" content="CHSB Dashboard App - Lorem ipsum dolor sit ametsed do eiusmod tempor incididunt ut labore et dolore magna aliqua. @swissborg"></meta>
-      <meta name="twitter:site" content="@swissborg"></meta>
-      <meta name="twitter:creator" content="@cortesdev"></meta>
+      />
       <meta name="pinterest" content="nopin" />
     </Head>
   );
