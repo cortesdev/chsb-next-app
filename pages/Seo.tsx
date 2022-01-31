3686900ef991c559517ac6153760b5b88e1ed500
Ricardo Côrtes <giralander@hotmail.com>
2022-01-31 18:00:23 +0100

    Add contentype

--- pages/Seo.tsx
+++ pages/Seo.tsx
@@ -14,6 +14,7 @@ const Seo: FC = () => {
       <title>
         CHSB Dashboard - Lorem ipsum dolor sit amet, consectetur adipiscing elit
       </title>
+      <meta content="text/html; charset=UTF-8" name="Content-Type" />
       <meta
         name="description"
         content="CHSB Dashboard App - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
